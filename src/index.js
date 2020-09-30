5b2e0811946ae108629a76702a7fe8af9ff61297
tomasdetloging <tomasdetloging@gmail.com>
2020-09-30 08:26:04 -0400

    add new styles

--- src/index.js
+++ src/index.js
@@ -2,8 +2,8 @@ import React from "react";
 import ReactDOM from "react-dom";
 import Main from "./views/Main";
 
-import "./assets/bootstrap.min.css";
-import "./assets/myStyles.css";
+import "./assets/css/black-dashboard-react.min.css";
+import "./assets/css/myStyles.css";
 import "font-awesome/css/font-awesome.min.css";
 
 ReactDOM.render(<Main />, document.getElementById("root"));
