63fc4f8db9cde0b617a963c60587507bfad55d61
tomasdetloging <tomasdetloging@gmail.com>
2021-02-11 22:28:48 -0400

    add perfectScroll

--- src/App.jsx
+++ src/App.jsx
@@ -10,33 +10,36 @@ import PaymentReports from "views/PaymentReports";
 //firebase
 import AuthWrapper from "components/auth/AuthWrapper";
 import OnlyAdminsWrapper from "components/auth/OnlyAdminsWrapper";
+import PerfectScrollWraper from "components/common/PerfectScrollWraper";
 
 export default function App() {
   return (
     <AuthWrapper>
       <OnlyAdminsWrapper>
-        <Router>
-          <Route exact path="/" render={(props) => <Main {...props} />} />
-          <Route
-            exact
-            path="/settings"
-            render={(props) => <Settings {...props} />}
-          />
-          <Route
-            exact
-            path="/courses"
-            render={(props) => <AllCourses {...props} />}
-          />
-          <Route
-            exact
-            path="/payment_reports"
-            render={(props) => <PaymentReports {...props} />}
-          />
-          <Route
-            path="/edit/:course"
-            render={(props) => <EditItems {...props} />}
-          />
-        </Router>
+        <PerfectScrollWraper>
+          <Router>
+            <Route exact path="/" render={(props) => <Main {...props} />} />
+            <Route
+              exact
+              path="/settings"
+              render={(props) => <Settings {...props} />}
+            />
+            <Route
+              exact
+              path="/courses"
+              render={(props) => <AllCourses {...props} />}
+            />
+            <Route
+              exact
+              path="/payment_reports"
+              render={(props) => <PaymentReports {...props} />}
+            />
+            <Route
+              path="/edit/:course"
+              render={(props) => <EditItems {...props} />}
+            />
+          </Router>
+        </PerfectScrollWraper>
       </OnlyAdminsWrapper>
     </AuthWrapper>
   );
